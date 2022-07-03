647b3223e0e6da095bb5e279461f3db290e75356
sonjungmin <jm981001@naver.com>
2022-07-03 14:55:19 +0900

    init commit

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+	public static void main(String[] args) {
+		int a = 10000; //돈
+		int b = 1000; //사과
+		
+		System.out.println(a/b);
+		System.out.println(b*3);
+		System.out.println(a-(b*5));
+		System.out.println((b*5)*0.01);
+		
+		
+	}
+	
+}
